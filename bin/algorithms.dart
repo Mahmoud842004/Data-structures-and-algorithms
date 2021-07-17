6b8746f9574240193c347797f987686e46ea59a1
unknown <bodyashhegab@gmail.com>
2021-07-17 05:52:56 +0200

    making complex multiplication algorithms

--- bin/algorithms.dart
+++ bin/algorithms.dart
@@ -1,5 +1,1 @@
-import 'package:algorithms/algorithms.dart' as algorithms;
 
-void main(List<String> arguments) {
-  print('Hello world: ${algorithms.calculate()}!');
-}
--- lib/algorithms.dart
+++ lib/algorithms.dart
@@ -1,3 +1,31 @@
-int calculate() {
-  return 6 * 7;
+void main(List<String> args) {
+  print(integermultiplication(m1: 1234, m2: 5678));
+}
+
+int integermultiplication({int m1, int m2}) {
+  var list1 = convertint(m1);
+  var finalresult = 0;
+  var passednumbers = 0;
+
+  for (var i = list1.length - 1; i >= 0; i--) {
+    var finalnumber = '';
+    for (var i = 1; i <= passednumbers; i++) {
+      finalnumber = '${finalnumber}0';
+    }
+    finalnumber = '${list1[i] * m2}$finalnumber';
+    finalresult = finalresult + int.parse(finalnumber);
+    passednumbers++;
+    print(finalnumber);
+  }
+  return finalresult;
+}
+
+List<int> convertint(int number) {
+  List<int> intlist = [];
+  var stringnumber = number.toString();
+  var stringlist = stringnumber.split('');
+  stringlist.forEach((element) {
+    intlist.add(int.parse(element));
+  });
+  return intlist;
 }
