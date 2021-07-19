8fd9b7c7006bcd9dce9f02540ca77217fc48e507
unknown <bodyashhegab@gmail.com>
2021-07-19 07:51:28 +0200

    learning merge sort

--- lib/algorithms.dart
+++ lib/algorithms.dart
@@ -1,33 +1,63 @@
+
 void main(List<String> args) {
-  print(summparis([1, 2, 4, 4,4], 8));
+  print(
+    multiplication(
+        number1:
+            '3141592653589793238462643383279502884197169399375105820974944592',
+        number2:
+            '2718281828459045235360287471352662497757247093699959574966967627'),
+  );
 }
 
-dynamic sumpairs(List list, int sum) {
-  var returnedlist = [];
-  for (var i = 0; i < list.length - 1; i++) {
-    for (var j = 0; j < list.length; j++) {
-      if (list[i] + list[j] == sum && i != j) {
-        returnedlist.add([list[i], list[j]]);
+List mergesort(List input) {
+  if (input.length == 1) {
+    return input;
+  }
+
+  var fristlist = mergesort(input.sublist(0, (input.length / 2).toInt()));
+  var secoundlist =
+      mergesort(input.sublist((input.length / 2).toInt(), input.length));
+  return merge(fristlist, secoundlist);
+}
+
+List merge(List fristlist, List secoundlist) {
+  var listlenght = fristlist.length + secoundlist.length;
+  var sortedlist = [];
+
+  var i = 0;
+  var j = 0;
+
+  for (var k = 0; k < listlenght; k++) {
+    if (i == fristlist.length) {
+      sortedlist.add(secoundlist[j]);
+      j = j + 1;
+    } else if (j == secoundlist.length) {
+      sortedlist.add(fristlist[i]);
+      i = i + 1;
+    } else {
+      if (fristlist[i] < secoundlist[j]) {
+        sortedlist.add(fristlist[i]);
+        i = i + 1;
+      } else {
+        sortedlist.add(secoundlist[j]);
+        j = j + 1;
       }
     }
   }
-  if (returnedlist.length == 0) {
-    return 'there is no items';
-  } else {
-    return returnedlist;
-  }
+  return sortedlist;
 }
 
-dynamic summparis(List list, int sum) {
-  var returnedlist = [];
-  for (var i = 0; i < list.length; i++) {
-    if (list.contains(sum - list[i]) && i != list.indexOf(sum - list[i])) {
-      returnedlist.add([list[i], sum - list[i]]);
-    }
-  }
-  if (returnedlist.isEmpty) {
-    return 'there is no paired item there sum is equel the result';
-  } else {
-    return returnedlist;
-  }
+BigInt multiplication({String number1, String number2}) {
+  var num1 = BigInt.parse(number1);
+  var num2 = BigInt.parse(number2);
+  var result = num1 * num2;
+  return result;
+}
+
+List<BigInt> convertnumber(BigInt number) {
+  var numberslist = [];
+  number.toString().split('').forEach((element) {
+    numberslist.add(BigInt.parse(element));
+  });
+  return numberslist;
 }
